6a4542ca91cd13ff72f10b1f8410b262a6368f4d
pandineer <pandineer@gmail.com>
2014-08-13 16:01:30 +0900

    Modified ex01_01

--- java8_training/text/ch01/ex01_01/Ex01.java
+++ java8_training/text/ch01/ex01_01/Ex01.java
@@ -5,21 +5,20 @@ import java.util.Arrays;
 public class Ex01 {
 
   public static void showPandas(String[] pandas) {
+    System.out.println("");
     for (int i = 0; i < pandas.length; i++) {
       System.out.println(i + ": " + pandas[i]);
     }
-
+    System.out.println("");
   }
 
   public static void main(String[] args) {
     System.out.println("Main thread name: " + Thread.currentThread().getName());
-    System.out.println("");
+
     String pandas[] = {"Lesser panda", "Panda", "Giant panda"};
     showPandas(pandas);
-    System.out.println("");
     Arrays.sort(pandas, new LengthComparator());
     System.out.println("Sorted!");
-    System.out.println("");
     showPandas(pandas);
   }
 }

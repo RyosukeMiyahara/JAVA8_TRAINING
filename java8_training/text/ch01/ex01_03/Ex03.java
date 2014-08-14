b2f340769ce36a54e5b3c91ded3bd6436fa9f1d7
pandineer <pandineer@gmail.com>
2014-08-14 15:42:56 +0900

    Implemented EX03 Lambda version

--- java8_training/text/ch01/ex01_03/Ex03.java
+++ java8_training/text/ch01/ex01_03/Ex03.java
@@ -32,10 +32,38 @@ public class Ex03 {
     }
   }
 
+  public static void getFilesByExtensionLambda(LinkedList<File> list, String directoryPath, String extension) {
+    File file = new File(directoryPath);
+    FilenameFilter extensionFilter = (path, name) -> {return name.endsWith(extension);};
+    String[] files = file.list(extensionFilter);
+    for(String oneFile: files) {
+      list.add(new File(directoryPath + System.getProperty("file.separator") + oneFile));
+    }
+
+    FileFilter directoryFilter = new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        return file.isDirectory();
+      }
+    };
+    File[] directories = file.listFiles(directoryFilter);
+    for(File oneFile: directories) {
+      getFilesByExtensionLambda(list, oneFile.toString(), extension);
+    }
+  }
+
   public static void main(String[] args) {
-    LinkedList<File> list = new LinkedList<File>();
-    getFilesByExtensionFilenameFilter(list, "D:\\10_development\\git\\java8_training", ".java");
-    for(File oneFile: list) {
+    LinkedList<File> listFilenameFilter = new LinkedList<File>();
+    getFilesByExtensionFilenameFilter(listFilenameFilter, "D:\\10_development\\git\\java8_training", ".java");
+    for(File oneFile: listFilenameFilter) {
+      System.out.println(oneFile);
+    }
+
+    System.out.println("");
+
+    LinkedList<File> listLambda = new LinkedList<File>();
+    getFilesByExtensionLambda(listLambda, "D:\\10_development\\git\\java8_training", ".java");
+    for(File oneFile: listLambda) {
       System.out.println(oneFile);
     }
   }

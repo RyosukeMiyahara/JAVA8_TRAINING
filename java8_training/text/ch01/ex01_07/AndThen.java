a954cc5f1c0b2bdf45001e7ef880a5f956f42325
pandineer <pandineer@gmail.com>
2014-08-21 09:00:23 +0900

    Started to implement EX01_07

---
+++ java8_training/text/ch01/ex01_07/AndThen.java
@@ -0,0 +1,17 @@
+package ch01.ex01_07;
+
+public class AndThen {
+
+  public static Runnable andThen(Runnable first, Runnable second) {
+    Runnable combined = ()->{
+      first.run();
+      second.run();
+    };
+    return combined;
+  }
+
+  public static void main(String[] args) {
+    Runnable combined = AndThen.andThen(()->System.out.println("I'm first!");, ()->System.out.println("I'm second!!";)};)
+  }
+
+}

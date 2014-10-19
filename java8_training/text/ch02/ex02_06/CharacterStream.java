0e61a66797d7235a8d4f423476e83715692aa349
pandineer <pandineer@gmail.com>
2014-10-19 15:03:20 +0900

    Refactored

--- java8_training/text/ch02/ex02_06/CharacterStream.java
+++ java8_training/text/ch02/ex02_06/CharacterStream.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class EX02_06 {
+public class CharacterStream {
 
+  /**
+   * Convert String to Stream<Character>
+   * @param s Convert this String to Stream<Character>
+   * @return Converted stream
+   */
   public static Stream<Character> characterStream(String s) {
     List<Character> result = new ArrayList<>();
     for (char c : s.toCharArray())
@@ -13,10 +18,13 @@ public class EX02_06 {
     return result.stream();
   }
 
+  /**
+   * Convert String to Stream<Character>, using stream
+   * @param s Convert this String to Stream<Character>
+   * @return Converted stream
+   */
   public static Stream<Character> characterStreamStreamVer(String s) {
-    Stream<Character> result = Stream.iterate(0, n->n+1).limit(s.length()).map(s::charAt);
-
-    return result;
+    return Stream.iterate(0, n->n+1).limit(s.length()).map(s::charAt);
   }
 
   public static void main(String[] args) {

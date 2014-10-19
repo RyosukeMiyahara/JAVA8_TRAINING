646e44f7fa276cf9aa8ea354762c7c3cb575302c
pandineer <pandineer@gmail.com>
2014-10-19 21:34:36 +0900

    Fixed indent

--- java8_training/text/ch02/ex02_12/CountShorterWordsTest.java
+++ java8_training/text/ch02/ex02_12/CountShorterWordsTest.java
@@ -11,15 +11,15 @@ public class CountShorterWordsTest {
 
   @Test
   public void countShorterWordsAtomicTest() {
-  Stream<String> parallelWords = Stream.of("1", "22", "22", "333", "333", "333", "4444", "4444", "4444", "4444");
-  AtomicInteger[] arrayAtomicInteger = CountShorterWords.countShorterWordsAtomic(4, parallelWords);
+    Stream<String> parallelWords = Stream.of("1", "22", "22", "333", "333", "333", "4444", "4444", "4444", "4444");
+    AtomicInteger[] arrayAtomicInteger = CountShorterWords.countShorterWordsAtomic(4, parallelWords);
 
-  AtomicInteger[] expected = {new AtomicInteger(0), new AtomicInteger(1), new AtomicInteger(2), new AtomicInteger(3)};
+    AtomicInteger[] expected = { new AtomicInteger(0), new AtomicInteger(1), new AtomicInteger(2), new AtomicInteger(3) };
 
-  for(int i = 0; i < expected.length; i++) {
-    if (expected[i].get() != arrayAtomicInteger[i].get()) {
-      fail("Index " + i + " does not match. Expected: " + expected[i] + " and actual: " + arrayAtomicInteger[i]);
+    for (int i = 0; i < expected.length; i++) {
+      if (expected[i].get() != arrayAtomicInteger[i].get()) {
+        fail("Index " + i + " does not match. Expected: " + expected[i] + " and actual: " + arrayAtomicInteger[i]);
+      }
     }
-  }
   }
 }

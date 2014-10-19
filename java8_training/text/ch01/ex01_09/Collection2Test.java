bbc0ad1ad06013b46ff4718639fbb35f528bbd9c
pandineer <pandineer@gmail.com>
2014-10-19 13:45:51 +0900

    Added test

--- java8_training/text/ch01/ex01_09/Collection2Test.java
+++ java8_training/text/ch01/ex01_09/Collection2Test.java
@@ -1,19 +1,25 @@
 package ch01.ex01_09;
 
+import static org.junit.Assert.*;
+
 import java.util.LinkedList;
 
+import org.junit.Test;
 
-public class Collection2Sample extends LinkedList<String> implements Collection2<String> {
+public class Collection2Test {
 
-  public static void main(String[] args) {
+  @Test
+  public void collection2Test() {
     Collection2Sample collection2Sample = new Collection2Sample();
     collection2Sample.add("Panda");
     collection2Sample.add("Dog");
     collection2Sample.add("Lesser Panda");
     collection2Sample.add("Cat");
     collection2Sample.add("Giant Panda");
+
+    LinkedList<String> filtered = new LinkedList<String>();
     collection2Sample.forEachIf(
-        string->System.out.println("*" + string + "*"),
+        string->filtered.add(string),
         string->{
           if (string.indexOf("Panda") != -1) {
             return true;
@@ -22,5 +28,6 @@ public class Collection2Sample extends LinkedList<String> implements Collection2
           }
         }
         );
+    assertEquals(filtered.size(), 3);
   }
 }

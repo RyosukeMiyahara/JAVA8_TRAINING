3ed759ea5491f5f8abfaf79d4bae215e5e075714
pandineer <pandineer@gmail.com>
2014-11-09 21:14:57 +0900

    Wrote answer

--- java8_training/text/ch03/ex03_10/README.md
+++ java8_training/text/ch03/ex03_10/README.md
@@ -6,3 +6,13 @@
     Image finalImage = transform(image, op.compose(Color::grayscale));
 
 UnaryOperator<T>のcomposeメソッドの戻り値型を注意深く調べなさい。なぜ、transformメソッドに対しては、適切ではないのでしょうか。関数合成に関しては、ストラクチャル型(structural type)とノミナル型(nominal type)のユーティリティに関して、何が言えますか。
+
+## 回答
+
+    default <V> Function<V, R> compose(Function<? super V, ? extends T> before) {
+      Objects.requireNonNull(before);
+      return (V v) -> apply(before.apply(v));
+    }
+
+    コンパイルエラー：型 Sample のメソッド transform(Image, UnaryOperator<Color>) は引数 (Image, Function<Color,Color>) に適用できません
+
--- java8_training/text/ch03/ex03_10/Sample.java
+++ java8_training/text/ch03/ex03_10/Sample.java
@@ -3,14 +3,36 @@ package ch03.ex03_10;
 import java.util.function.UnaryOperator;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 public class Sample {
 
+  /**
+   * Transform image using provided oeration
+   * This function is written in text
+   * @param in image to be transformed
+   * @param f operation to transform
+   * @return transformed image
+   */
+  public static Image transform(Image in, UnaryOperator<Color> f) {
+    int width = (int) in.getWidth();
+    int height = (int) in.getHeight();
+    WritableImage out = new WritableImage(width, height);
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        out.getPixelWriter().setColor(x,  y, f.apply(in.getPixelReader().getColor(x, y)));
+      }
+    }
+    return out;
+  }
+
   public static void main(String[] args) {
     Image image = new Image("test");
-    UnaryOperator op = Color::brighter;
-    Image finalImage = transform(image, op.compose(Color::grayscale));)
+    UnaryOperator<Color> op = Color::brighter;
+    // Image finalImage1 = transform(image, op.compose(Color::grayscale));)
+    @SuppressWarnings("unused")
+    Image finalImage2 = transform(image, op);
   }
 
 }

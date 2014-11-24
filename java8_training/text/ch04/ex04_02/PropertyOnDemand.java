941b39986d92eb7d1b0ab77c8fa99ccde3e58f0f
pandineer <pandineer@gmail.com>
2014-11-24 13:40:34 +0900

    Completed ex4-2

---
+++ java8_training/text/ch04/ex04_02/PropertyOnDemand.java
@@ -0,0 +1,48 @@
+package ch04.ex04_02;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class PropertyOnDemand {
+  private int normalInt = 0;
+  private IntegerProperty property = null;
+
+  /**
+   * Return integer value
+   * @return if property is created, return propert.get()
+   *         if not, return normal int value
+   */
+  public int getValue() {
+    if (property == null) {
+      return normalInt;
+    } else {
+      return property.get();
+    }
+  }
+
+  /**
+   * Set integer value
+   * @param newValue if property is created, set new value to property
+   *                 if not, set it to normal int
+   */
+  public void setValue(int newValue) {
+    if (property == null) {
+      normalInt = newValue;
+    } else {
+      property.set(newValue);
+    }
+  }
+
+  /**
+   * Return property. If not created yet, create and return it
+   * @return IntegerProperty
+   */
+  public IntegerProperty getProperty() {
+    if (property == null) {
+      property = new SimpleIntegerProperty(normalInt);
+      return property;
+    } else {
+      return property;
+    }
+  }
+}

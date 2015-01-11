ef2af6300e6e529a8908f0ce6aaae453936379ab
pandineer <pandineer@gmail.com>
2015-01-11 14:46:02 +0900

    Changed from Thread to ExecutorService

--- java8_training/text/ch06/ex06_01/MaxLengthManager.java
+++ java8_training/text/ch06/ex06_01/MaxLengthManager.java
@@ -3,10 +3,14 @@ package ch06.ex06_01;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MaxLengthManager {
   public AtomicReference<Integer> maxLength = new AtomicReference<Integer>();
+  public static final int THREAD_NUM = 3;
 
   public static void main(String[] args) {
     MaxLengthManager maxLengthManager = new MaxLengthManager();
@@ -21,36 +25,21 @@ public class MaxLengthManager {
     }
     String[] words = contents.split("[\\P{L}]+");
 
-    Thread thread1 = new Thread(() -> {
-      for (int i = 0; i < words.length / 3; i++) {
-        maxLengthManager.maxLength.accumulateAndGet(words[i].length(), Math::max);
-      }
-    });
-
-    Thread thread2 = new Thread(() -> {
-      for (int i = words.length / 3; i < (words.length / 3) * 2; i++) {
-        maxLengthManager.maxLength.accumulateAndGet(words[i].length(), Math::max);
-      }
-    });
-
-    Thread thread3 = new Thread(() -> {
-      for (int i = (words.length / 3) * 2; i < words.length; i++) {
-        maxLengthManager.maxLength.accumulateAndGet(words[i].length(), Math::max);
-      }
-    });
-
-    thread1.start();
-    thread2.start();
-    thread3.start();
-
-    try {
-      thread1.join();
-      thread2.join();
-      thread3.join();
-    } catch (Exception e) {
-      System.err.println("join failed");
-      e.printStackTrace();
+    ExecutorService pool = Executors.newCachedThreadPool();
+    for (int i = 0; i < THREAD_NUM; i++) {
+      int offset = i;
+      pool.submit(() -> {
+        for (int j = words.length / THREAD_NUM * offset; j < words.length / THREAD_NUM * (offset + 1); j++) {
+          maxLengthManager.maxLength.accumulateAndGet(words[j].length(), Math::max);
+        }
+      });
     }
+   pool.shutdown();
+   try {
+     pool.awaitTermination(10, TimeUnit.SECONDS);
+   } catch (Exception e) {
+     e.printStackTrace();
+   }
 
     System.out.println("Result: " + maxLengthManager.maxLength.get());
   }

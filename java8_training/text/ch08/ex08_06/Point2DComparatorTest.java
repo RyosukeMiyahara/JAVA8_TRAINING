5ee7fbb386bad7d4fbafb123aac475a91b406655
pandineer <pandineer@gmail.com>
2015-03-07 11:33:39 +0900

    Refactored

--- java8_training/text/ch08/ex08_06/Point2DComparatorTest.java
+++ java8_training/text/ch08/ex08_06/Point2DComparatorTest.java
@@ -3,7 +3,6 @@ package ch08.ex08_06;
 import static org.junit.Assert.*;
 
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -23,25 +22,6 @@ public class Point2DComparatorTest {
     Arrays.sort(point2DArray, Comparator.comparingDouble(Point2D::getX).thenComparingDouble(Point2D::getY));
 
     assertArrayEquals(expected, point2DArray);
-  }
-
-  @Test
-  public void Rectangle2DComparatorTest() {
-    Rectangle2D oneOneOneOne = new Rectangle2D.Double(1, 1, 1, 1);
-    Rectangle2D oneOneOneTwo = new Rectangle2D.Double(1, 1, 1, 2);
-    Rectangle2D twoOneOneOne = new Rectangle2D.Double(2, 1, 1, 1);
-    Rectangle2D oneOneThreeOne = new Rectangle2D.Double(1, 1, 3, 1);
-
-    Rectangle2D[] rectangle2DArray = {twoOneOneOne, oneOneThreeOne, oneOneOneOne, oneOneOneTwo};
-    Rectangle2D[] expected = {oneOneOneOne, oneOneOneTwo, oneOneThreeOne, twoOneOneOne};
-
-    Arrays.sort(rectangle2DArray, Comparator
-        .comparingDouble(Rectangle2D::getX)
-        .thenComparingDouble(Rectangle2D::getY)
-        .thenComparingDouble(Rectangle2D::getWidth)
-        .thenComparingDouble(Rectangle2D::getHeight));
-
-    assertArrayEquals(expected, rectangle2DArray);
   }
 
 }

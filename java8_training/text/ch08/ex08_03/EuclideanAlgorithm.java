f3b4cd3e1d656c32d40ba585a800b0dd73703b7a
pandineer <pandineer@gmail.com>
2015-03-07 09:10:45 +0900

    In progress...

--- java8_training/text/ch08/ex08_03/EuclideanAlgorithm.java
+++ java8_training/text/ch08/ex08_03/EuclideanAlgorithm.java
@@ -1,5 +1,7 @@
 package ch08.ex08_03;
 
+//数学的な（負ではない）余り、とは？？？
+
 public class EuclideanAlgorithm {
 
   public static int gcd_percent(int a, int b) {
@@ -15,9 +17,27 @@ public class EuclideanAlgorithm {
     }
     return gcd_floorMod(b, Math.floorMod(a, b));
   }
+
+  public static int gcd_rem(int a, int b) {
+    System.out.println("a=" + a);
+    System.out.println("b=" + b);
+    if (b == 0) {
+      return a;
+    }
+    return gcd_rem(b, rem(a, b));
+  }
+
+  public static int rem(int a, int b) {
+
+    System.out.println("rem=" + ((a % b) + b) % b);
+    return ((a % b) + b) % b;
+    // return (a - (a/b)*b);
+  }
+
   public static void main(String[] args) {
     System.out.println(gcd_percent(40, -50));
     System.out.println(gcd_floorMod(-50, -40));
+    System.out.println(gcd_rem(40, -50));
   }
 
 }
